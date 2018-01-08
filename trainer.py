0475d958effd8a78ac8bc954eedca977bd88d95f
Cheick Keita <kcheick@gmail.com>
2018-01-08 08:50:15 -0800

    added  data augmentation and nvidia dnn

--- trainer.py
+++ trainer.py
@@ -2,103 +2,148 @@ import csv
 import cv2
 import numpy as np
 
-drivingLogPath = './windows_sim/recording/driving_log.csv'
-images = []
-measurements = []
-# csv format 
-# center image, Left Image, Right Imge, Steering Angle, Throttle, Break, Speed
-with open(drivingLogPath) as csvFile:
-    reader = csv.reader(csvFile)
-    for line in reader:
-        center_image = line[0]
-        images.append(cv2.imread(center_image))
-        measurements.append(float(line[3]))
-
-
-X_train = np.array(images)
-y_train = np.array(measurements)
 
 # building a regression model for the sterring value 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Flatten, Dense, Lambda, convolutional
 import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Convolution2D
+from keras.layers import Conv2D, MaxPooling2D, Cropping2D
+from keras import backend as K
+from pyxtension.streams import stream as pyStream
+
+
+
+
+
+def readData(drivingLogPath='./windows_sim/recording/driving_log.csv', augment=(lambda x,y: ([x],[y]) )):
+    images = []
+    measurements = []
+    # csv format 
+    # center image, Left Image, Right Imge, Steering Angle, Throttle, Break, Speed
+    with open(drivingLogPath) as csvFile:
+        reader = csv.reader(csvFile)
+        for line in reader:
+            center_image = line[0]
+            image = cv2.imread(center_image)
+            measurement = float(line[3])
+            augmented_images, augemented_measurement = augment(image, measurement)
+            images.extend(augmented_images)
+            measurements.extend(augemented_measurement)
+
+def readDataGen(drivingLogPath='./windows_sim/recording/driving_log.csv'):
+    # csv format 
+    # center image, Left Image, Right Imge, Steering Angle, Throttle, Break, Speed
+    with open(drivingLogPath) as csvFile:
+        reader = csv.reader(csvFile)
+        correction = 0.2
+        for line in reader:
+            center_image    = cv2.imread(line[0]) 
+            left_image      = cv2.imread(line[1]) 
+            right_image     = cv2.imread(line[2]) 
+            measurement     = float(line[3])
+            yield [
+                    (center_image, measurement),
+                    (left_image, measurement + correction),
+                    (right_image, measurement - correction)
+                  ]
+            
+
+def augment_data(image, measurement):
+    return [(image, measurement), (np.fliplr(image), - measurement)]
+
+
+rd = pyStream(readDataGen()).flatMap().map(lambda data: augment_data(data[0], data[1])).flatMap()
+print(type(rd))
+xt, yt = zip(*rd)
+X_train, y_train =  (np.array((xt)), np.array((yt)))
+print(type(X_train[0]))
+print(X_train[0].shape)
+# print(y_train[0])
+# exit()
+
+# X_train, y_train = 
+
+
+# X_train, y_train = readData(augment=lambda image, meas: ([image, np.fliplr(image)], [meas, -meas]))
+#print(y_train)
+#print(y_train[1])
+
 
 
 def simpleDnn(X_train, y_train):
     model = Sequential()
     # normalizing and mean centering
     model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=X_train[0].shape))
+    model.add(Cropping2D(cropping=((70,25), (0,0))))
     model.add(Flatten())
     model.add(Dense(1))
-
     model.compile(loss=keras.losses.mean_squared_error, optimizer= keras.optimizers.adam())
     model.fit(X_train, y_train, validation_split= 0.2, shuffle=True, epochs=5)
 
     return model
     model.save('model.h5')
 
+
+
 def lenet(X_train, y_train):
-    batch_size = 128
+    batch_size = 32
     num_classes = 10
     epochs = 12
 
-    # input image dimensions
-    img_rows, img_cols = X_train[0].shape
+    model = Sequential()
+    model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=X_train[0].shape))
+    model.add(Cropping2D(cropping=((70,25), (0,0))))
+    model.add(Convolution2D(6, kernel_size=(5, 5), activation='relu'))
+    model.add(MaxPooling2D())
+    model.add(Convolution2D(6, (5, 5), activation='relu'))
+    model.add(MaxPooling2D())
+    model.add(Flatten())
+    model.add(Dense(128))
+    model.add(Dense(84))
+    model.add(Dense(1))
 
-    # the data, shuffled and split between train and test sets
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    model.compile(
+        loss=keras.losses.mean_squared_error, 
+        optimizer= keras.optimizers.adam())
+    model.fit(X_train, y_train, validation_split= 0.2, shuffle=True, epochs=epochs, batch_size=batch_size)
+    return model
 
-    if K.image_data_format() == 'channels_first':
-        x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
-        x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
-        input_shape = (1, img_rows, img_cols)
-    else:
-        x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-        x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-        input_shape = (img_rows, img_cols, 1)
 
-    x_train = x_train.astype('float32')
-    x_test = x_test.astype('float32')
-    x_train /= 255
-    x_test /= 255
-    print('x_train shape:', x_train.shape)
-    print(x_train.shape[0], 'train samples')
-    print(x_test.shape[0], 'test samples')
-
-    # convert class vectors to binary class matrices
-    y_train = keras.utils.to_categorical(y_train, num_classes)
-    y_test = keras.utils.to_categorical(y_test, num_classes)
+def nvidia(X_train, y_train):
+    batch_size = 32
+    num_classes = 10
+    epochs = 5
 
     model = Sequential()
-    model.add(Conv2D(32, kernel_size=(3, 3),
-                    activation='relu',
-                    input_shape=input_shape))
-    model.add(Conv2D(64, (3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
+    model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=X_train[0].shape))
+    model.add(Cropping2D(cropping=((70,25), (0,0))))
+    model.add(Convolution2D(24, kernel_size=(5, 5), subsample=(2,2), activation='relu'))
+    model.add(Convolution2D(36, kernel_size=(5, 5), subsample=(2,2), activation='relu'))
+    model.add(Convolution2D(48, kernel_size=(5, 5), subsample=(2,2), activation='relu'))
+    model.add(Convolution2D(64, (3, 3), activation='relu'))
+    model.add(Convolution2D(64, (3, 3), activation='relu'))
     model.add(Flatten())
-    model.add(Dense(128, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(num_classes, activation='softmax'))
+    model.add(Dense(100))
+    model.add(Dense(50))
+    model.add(Dense(10))
+    model.add(Dense(1))
 
-    model.compile(loss=keras.losses.categorical_crossentropy,
-                optimizer=keras.optimizers.Adadelta(),
-                metrics=['accuracy'])
+    model.compile(
+        loss=keras.losses.mean_squared_error, 
+        optimizer= keras.optimizers.adam())
+    model.fit(X_train, y_train, validation_split= 0.2, shuffle=True, epochs=epochs, batch_size=batch_size)
+    return model 
 
-    model.fit(x_train, y_train,
-            batch_size=batch_size,
-            epochs=epochs,
-            verbose=1,
-            validation_data=(x_test, y_test))
-    score = model.evaluate(x_test, y_test, verbose=0)
-    print('Test loss:', score[0])
-    print('Test accuracy:', score[1])
+nvidia(X_train, y_train).save("nvida.h5")
+#lenet(X_train, y_train)
+#simpleDnn(X_train, y_train)
 
 
 
+#simple = simpleDnn()
+#simple.save('simple.h5')
 
-
-
-simple = simpleDnn()
-simple.save('simple.h5')
 
